8904d91042c37a6a4b93ec1098f96e1500c724eb
Ben Kumar <benkumar@my-cpe.com>
2026-04-01 21:57:24 +0530

    Replace bolt icon with MyCPE ONE logo

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return send_file("index.html")
+
+@app.route("/<path:filename>")
+def static_files(filename):
+    return send_from_directory(".", filename)
 
 @app.route("/health")
 def health():
